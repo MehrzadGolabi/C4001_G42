7ba8c877db2503b4d6a6e6b9c571c31acecb765f
MehrzadGolabi <96944259+MehrzadGolabi@users.noreply.github.com>
2022-01-09 23:53:41 +0330

    Update kahesh fasele khat ha be 1 khat.cpp

--- kahesh fasele khat ha be 1 khat.cpp
+++ kahesh fasele khat ha be 1 khat.cpp
@@ -7,68 +7,70 @@
 using namespace std;
 
 string line;
+
 void copyy();
+
 void update();
+
 void deletspace();
+
 void deletline();
+
 void capital();
 
 
-int main()
-{
+int main() {
     getch();
     return 0;
 }
+
 void copyy()  //copy youefile to ourfile
 {
-ifstream input("yourfile.txt");
-ofstream output("temp.txt");
-while(getline(input,line))
-output<<line<<"\n";
-input.close();
-output.close();
+    ifstream input("yourfile.txt");
+    ofstream output("temp.txt");
+    while (getline(input, line))
+        output << line << "\n";
+    input.close();
+    output.close();
 }
+
 void update()  //tabdil yourfile be ourfile
 {
-ifstream input("ourfile.txt");
-ofstream output("temp.txt");
-while(getline(input,line))
-output<<line<<"\n";
-input.close();
-output.close();
+    ifstream input("ourfile.txt");
+    ofstream output("temp.txt");
+    while (getline(input, line))
+        output << line << "\n";
+    input.close();
+    output.close();
 }
+
 void deletspace()  // hazfe space ezafe
 {
- unsigned i=0;
-ifstream input("temp.txt");
-ofstream output("ourfile.txt");
-if(input.is_open())
-{
-while(!input.eof())
-{
-getline(input,line);
-for(i=0;i<line.length();i++)
-{
-while(line[i]==' ')
-{
-i++;
+    unsigned i = 0;
+    ifstream input("temp.txt");
+    ofstream output("ourfile.txt");
+    if (input.is_open()) {
+        while (!input.eof()) {
+            getline(input, line);
+            for (i = 0; i < line.length(); i++) {
+                while (line[i] == ' ') {
+                    i++;
+                }
+            }
+        }
+    }
 }
-}
-}
-}
-}
+
 void deletline()  // hazfe khate ezafi
 {
-ifstream input("temp.txt");
-ofstream output("ourfile.txt");
-if(input.is_open())
-{
-while(getline(input,line))
-{
-if(line.length()==0)
-continue;
-output<<line;
-output<<"\n";
-}
-}
-}
+    ifstream input("temp.txt");
+    ofstream output("ourfile.txt");
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            if (line.length() == 0)
+                continue;
+            output << line;
+            output << "\n";
+        }
+    }
+} 
