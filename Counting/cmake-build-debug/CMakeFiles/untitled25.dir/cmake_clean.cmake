d8dbc3a04d97ecd913dce0098011cc9f9274c0ca
MehrzadGolabi <96944259+MehrzadGolabi@users.noreply.github.com>
2022-01-09 23:03:57 +0330

    `

---
+++ Counting/cmake-build-debug/CMakeFiles/untitled25.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled25.dir/main.cpp.o"
+  "untitled25"
+  "untitled25.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled25.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
